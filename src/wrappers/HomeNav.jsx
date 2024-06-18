a6ac94086278d9487280af234f3c35285411a9f3
jeeva m <jeelion22@gmail.com>
2024-06-19 00:01:59 +0530

    moved nav items to the end

--- src/wrappers/HomeNav.jsx
+++ src/wrappers/HomeNav.jsx
@@ -20,7 +20,7 @@ const HomeNav = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link
                   className="nav-link active"
