dfd4052e98330b26101616fb643c5ddd78fe296b
jeeva m <jeelion22@gmail.com>
2024-06-25 23:58:33 +0530

    added plot for contributions

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -35,7 +35,7 @@ const AdminDashboard = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
+        <div className="col-md-12 col-lg-6 col-sm-12">
           <div style={{ position: "relative" }}>
             <ResponsiveContainer
               className="border rounded pt-1"
@@ -90,6 +90,64 @@ const AdminDashboard = () => {
             >
               <p>Active Users: {data.totalActiveUsers}</p>
               <p>Inactive Users: {data.totalNonActiveUsers}</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-12 col-lg-6 col-sm-12">
+          <div style={{ position: "relative" }}>
+            <ResponsiveContainer
+              className="border rounded pt-1"
+              width="100%"
+              height={400}
+            >
+              <BarChart
+                width={730}
+                height={250}
+                data={data.contributionData}
+                barSize={50}
+                barGap={6}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                  dataKey="date"
+                  label={{ value: "Date", position: "insideBottom", offset: 0 }}
+                />
+                <YAxis
+                  label={{
+                    value: "No. of contributions by contributor",
+                    angle: -90,
+                    // position: "insideLeft",
+                    offset: "0",
+                  }}
+                />
+                <Tooltip />
+                <Legend verticalAlign="top" height={36} />
+
+                <ReferenceLine y={0} stroke="#000" />
+                <Bar dataKey="totalUploads" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
+            <div
+              className="text-body-secondary"
+              style={{
+                position: "absolute",
+                top: 10,
+                right: 10,
+                padding: "10px",
+                backgroundColor: "white",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <p>Total rescued: {data.totalRescued}</p>
+              <p>Total non-rescued: {data.totalNotRescued}</p>
             </div>
           </div>
         </div>
