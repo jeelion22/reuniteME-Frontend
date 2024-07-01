43b70750efad245b98b9547be31d60b6cd0a43f9
jeeva m <jeelion22@gmail.com>
2024-07-01 17:05:10 +0530

    added responsive by bootstrap

--- src/components/admin/AdminForgotPassword.jsx
+++ src/components/admin/AdminForgotPassword.jsx
@@ -1,3 +1,4 @@
+import "../../styles/AdminForgotPassword.css";
 import { useState } from "react";
 
 import { userForgotPasswordValidationSchema } from "../../validataionSchema/userForgotPasswordValidationScheme";
@@ -73,54 +74,59 @@ const AdminForgotPassword = () => {
         >
           {(formik) => (
             <form onSubmit={formik.handleSubmit}>
-              <div className="row justify-content-center text-center mt-4">
-                <div className="col-md-6">
-                  <h4>Forgot Password?</h4>
-                </div>
-              </div>
-
-              <div className="row justify-content-center mt-3">
-                <div className="col-md-6">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter your registered email address"
-                    aria-label="email"
-                    id="email"
-                    {...formik.getFieldProps("email")}
-                  />
-
-                  {formik.touched.email && formik.errors.email ? (
-                    <div className="email mt-2 text-danger">
-                      {formik.errors.email}
+              <div className="container">
+                <div className="row">
+                  <div className="col border rounded p-4 m-4 admin-forgot-password">
+                    <div className="row justify-content-center text-center mt-4">
+                      <div className="col">
+                        <h4>Forgot Password?</h4>
+                      </div>
                     </div>
-                  ) : null}
-                </div>
-              </div>
 
-              <div className="row justify-content-center mt-3">
-                <div className="col-md-6">
-                  <div className="d-flex justify-content-end">
-                    <button type="submit" className="btn btn-primary">
-                      Send Password Reset Link
-                    </button>
-                  </div>
-                </div>
-              </div>
+                    <div className="row justify-content-center mt-3">
+                      <div className="col-md-6">
+                        <input
+                          type="email"
+                          className="form-control"
+                          placeholder="Enter your registered email address"
+                          aria-label="email"
+                          id="email"
+                          {...formik.getFieldProps("email")}
+                        />
 
-              <div className="row justify-content-center mt-5">
-                <div className="col-md-6 text-center">
-                  <span>or</span>
-                </div>
-              </div>
+                        {formik.touched.email && formik.errors.email ? (
+                          <div className="email mt-2 text-danger">
+                            {formik.errors.email}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>
 
-              <div className="row justify-content-center mt-5">
-                <div className="col-md-3 d-flex">
-                  <div className="create-account">
-                    <span>For login your account? </span>
-                    <span>
-                      <Link to={"/admins/login"}>Click Here</Link>
-                    </span>
+                    <div className="row justify-content-center mt-3">
+                      <div className="col-md-6">
+                        <div className="d-flex justify-content-end">
+                          <button
+                            type="submit"
+                            className="btn btn-outline-primary"
+                          >
+                            Send Password Reset Link
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row justify-content-center m-4">
+                      <div className="col-md-6 text-center">
+                        <span>or</span>
+                      </div>
+                    </div>
+
+                    <div className="row justify-content-center text-center">
+                      <div className="col-md-6">
+                        <span>For login your account? </span>{" "}
+                        <Link to={"/admins/login"}>Click Here</Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
