25cc011eeaa895c973ec9b9c1e9898720bc22c8d
jeeva m <jeelion22@gmail.com>
2024-07-03 03:56:54 +0530

    changed baseURL for service instances

--- services/instance.js
+++ services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5001/api";
+const baseURL = "https://reuniteme-backend.onrender.com/api";
 
 // create an axios instance
 const instance = axios.create({
