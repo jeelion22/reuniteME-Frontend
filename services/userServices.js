7eb97641a03c618e96292107e58eef125ff9c35b
jeeva m <jeelion22@gmail.com>
2024-07-05 03:33:25 +0530

    changed method

--- services/userServices.js
+++ services/userServices.js
@@ -23,7 +23,7 @@ const userServices = {
     return await protectedInstance.get("/users/me");
   },
   logout: async () => {
-    return await protectedInstance.post("/users/logout");
+    return await protectedInstance.get("/users/logout");
   },
 
   getImage: async (imageId) => {
@@ -100,7 +100,7 @@ const userServices = {
     return await protectedInstance.get("admins/me");
   },
   adminLogout: async () => {
-    return await protectedInstance.post("/admins/logout");
+    return await protectedInstance.get("/admins/logout");
   },
 
   adminGetAllUsers: async () => {
