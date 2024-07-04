9b0a572332e0d66b43be2c1ff15f762b67ad54da
jeeva m <jeelion22@gmail.com>
2024-07-04 10:10:45 +0530

    modified error message

--- src/validataionSchema/userLoginValidation.js
+++ src/validataionSchema/userLoginValidation.js
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 export const userLoginValidation = Yup.object({
   email: Yup.string()
-    .email("Invalid email address")
-    .required("Email address should not empty."),
-  password: Yup.string().required("Password should not empty."),
+    .email("** Invalid email address")
+    .required("* Email address should not empty."),
+  password: Yup.string().required("* Password should not empty."),
 });
