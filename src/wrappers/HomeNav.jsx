c739c10f3763ba3e25f732022e870e5728eeb254
jeeva <jeelion22@gmail.com>
2024-11-11 18:41:28 +0530

    add link active feature, nav link add with h6 tag

--- src/wrappers/HomeNav.jsx
+++ src/wrappers/HomeNav.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import "../styles/HomeNav.css";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 const HomeNav = () => {
+  const location = useLocation();
+  const [active, setActive] = useState("");
+
+  useEffect(() => {
+    setActive(location.pathname);
+  }, [location.pathname]);
+
+  function activateLink(currentLink) {
+    setActive(currentLink);
+  }
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg home-dashboard-nav">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            ReUniteME
+            <h4> ReUniteME</h4>
           </Link>
           <button
             className="navbar-toggler"
@@ -23,21 +35,42 @@ const HomeNav = () => {
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link
-                  className="nav-link active "
+                  className={`nav-link ${
+                    active === "/users/register" ? "active" : ""
+                  }`}
                   aria-current="page"
                   to="/users/register"
+                  onClick={() => {
+                    activateLink("/users/register");
+                  }}
                 >
-                  Register
+                  <h6>Register</h6>
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/users/login">
-                  Login
+                <Link
+                  className={`nav-link ${
+                    active === "/users/login" ? "active" : ""
+                  }`}
+                  to="/users/login"
+                  onClick={() => {
+                    activateLink("/users/login");
+                  }}
+                >
+                  <h6>Login</h6>
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/admins/login">
-                  Admin Login
+                <Link
+                  className={`nav-link ${
+                    active === "/admins/login" ? "active" : ""
+                  }`}
+                  to="/admins/login"
+                  onClick={() => {
+                    activateLink("/admins/login");
+                  }}
+                >
+                  <h6>Admin Login</h6>
                 </Link>
               </li>
             </ul>
